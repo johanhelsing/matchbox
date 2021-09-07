c1cd5f6b0146a5fc1cd4136d40b18249444e1f4e
Johan Klokkhammer Helsing <johanhelsing@gmail.com>
2021-09-07 17:11:57 +0200

    refactor!: Make ggrs dependency optional

--- matchbox_socket/src/lib.rs
+++ matchbox_socket/src/lib.rs
@@ -1,5 +1,7 @@
+#[cfg(feature = "ggrs")]
 mod ggrs_socket;
 mod webrtc_socket;
 
-pub use ggrs_socket::*;
+#[cfg(feature = "ggrs")]
+pub use ggrs_socket::WebRtcNonBlockingSocket;
 pub use webrtc_socket::WebRtcSocket;

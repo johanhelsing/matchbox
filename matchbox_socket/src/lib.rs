7dc8b9998e3fcb2033a1d2dec1f607416987d847
Johan Klokkhammer Helsing <johanhelsing@gmail.com>
2021-09-11 07:59:15 +0200

    refactor: use ggrs-socket feature name instead of lib name

--- matchbox_socket/src/lib.rs
+++ matchbox_socket/src/lib.rs
@@ -1,7 +1,7 @@
-#[cfg(feature = "ggrs")]
+#[cfg(feature = "ggrs-socket")]
 mod ggrs_socket;
 mod webrtc_socket;
 
-#[cfg(feature = "ggrs")]
+#[cfg(feature = "ggrs-socket")]
 pub use ggrs_socket::WebRtcNonBlockingSocket;
 pub use webrtc_socket::WebRtcSocket;

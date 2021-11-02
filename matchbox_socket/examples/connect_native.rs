b6216111bb207dacfd5236aa44aefb0aa312309e
Johan Klokkhammer Helsing <johanhelsing@gmail.com>
2021-11-02 11:32:46 +0100

    docs: Send and receive messages in native example

--- matchbox_socket/examples/connect_native.rs
+++ matchbox_socket/examples/connect_native.rs
@@ -1,12 +1,35 @@
+use log::info;
 use matchbox_socket::WebRtcSocket;
+use tokio::select;
 
 #[tokio::main]
 async fn main() {
     pretty_env_logger::init();
 
-    println!("Connecting to matchbox");
-    let (_socket, loop_fut) = WebRtcSocket::new("ws://localhost:3536/native_example_room");
+    info!("Connecting to matchbox");
+    let (mut socket, loop_fut) = WebRtcSocket::new("ws://localhost:3536/native_example_room");
 
-    loop_fut.await;
-    println!("Done");
+    tokio::pin!(loop_fut);
+
+    loop {
+        let packets = socket.receive();
+
+        for (peer, packet) in packets {
+            info!("Received from {:?}: {:?}", peer, packet);
+        }
+
+        select! {
+            peers = socket.wait_for_peers(1) => {
+                info!("Found a peer {:?}", peers);
+                let peer = &peers[0];
+                let packet = "hello friend!".as_bytes().to_vec().into_boxed_slice();
+                socket.send(packet, peer);
+            },
+            _ = &mut loop_fut => {
+                break;
+            }
+        }
+    }
+
+    info!("Done");
 }

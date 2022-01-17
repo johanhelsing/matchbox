97e2935ceacd5f02706d392f0a6056e84ef23e6f
Johan Klokkhammer Helsing <johanhelsing@gmail.com>
2022-01-17 12:18:00 +0100

    Use strings as peer addresses

--- matchbox_socket/src/ggrs_socket.rs
+++ matchbox_socket/src/ggrs_socket.rs
@@ -1,61 +1,36 @@
 use futures::Future;
 use ggrs::{PlayerType, UdpMessage};
-use std::{
-    collections::{hash_map::DefaultHasher, HashMap},
-    hash::{Hash, Hasher},
-    net::{Ipv6Addr, SocketAddr},
-    pin::Pin,
-};
+use std::pin::Pin;
 
 use crate::WebRtcSocket;
 
 #[derive(Debug)]
 pub struct WebRtcNonBlockingSocket {
     socket: WebRtcSocket,
-    fake_socket_addrs: HashMap<String, SocketAddr>,
-    fake_socket_addrs_reverse: HashMap<SocketAddr, String>,
 }
 
 impl WebRtcNonBlockingSocket {
     #[must_use]
     pub fn new<T: Into<String>>(room_url: T) -> (Self, Pin<Box<dyn Future<Output = ()>>>) {
         let (socket, message_loop) = WebRtcSocket::new(room_url);
-        (
-            Self {
-                socket,
-                fake_socket_addrs: Default::default(),
-                fake_socket_addrs_reverse: Default::default(),
-            },
-            message_loop,
-        )
+        (Self { socket }, message_loop)
     }
 
-    pub async fn wait_for_peers(&mut self, peers: usize) {
-        let new_peers = self.socket.wait_for_peers(peers).await;
-
-        for id in new_peers {
-            let fake_addr = make_fake_socket_addr(&id);
-            self.fake_socket_addrs.insert(id.clone(), fake_addr);
-            self.fake_socket_addrs_reverse.insert(fake_addr, id);
-        }
+    pub async fn wait_for_peers(&mut self, peers: usize) -> Vec<String> {
+        self.socket.wait_for_peers(peers).await
     }
 
-    pub fn accept_new_connections(&mut self) {
-        let new_peers = self.socket.accept_new_connections();
-        for peer in new_peers {
-            self.handle_new_peer_id(peer);
-        }
+    pub fn accept_new_connections(&mut self) -> Vec<String> {
+        self.socket.accept_new_connections()
     }
 
-    pub fn connected_peers(&self) -> Vec<SocketAddr> {
-        self.socket
-            .connected_peers()
-            .iter()
-            .map(|id| *self.fake_socket_addrs.get(id).unwrap())
-            .collect()
+    #[must_use]
+    pub fn connected_peers(&self) -> Vec<String> {
+        self.socket.connected_peers()
     }
 
-    pub fn players(&self) -> Vec<PlayerType> {
+    #[must_use]
+    pub fn players(&self) -> Vec<PlayerType<String>> {
         // needs to be consistent order across all peers
         let mut ids = self.socket.connected_peers();
         ids.push(self.socket.id().to_owned());
@@ -65,53 +40,26 @@ impl WebRtcNonBlockingSocket {
                 if id == self.socket.id() {
                     PlayerType::Local
                 } else {
-                    let addr = *self.fake_socket_addrs.get(id).unwrap();
-                    PlayerType::Remote(addr)
+                    PlayerType::Remote(id.to_owned())
                 }
             })
             .collect()
     }
-
-    fn get_or_create_fake_addr(&mut self, id: &str) -> SocketAddr {
-        match self.fake_socket_addrs.get(id) {
-            Some(fake_addr) => *fake_addr,
-            None => self.handle_new_peer_id(id.to_string()),
-        }
-    }
-
-    fn handle_new_peer_id(&mut self, id: String) -> SocketAddr {
-        let fake_addr = make_fake_socket_addr(&id);
-        self.fake_socket_addrs.insert(id.clone(), fake_addr);
-        self.fake_socket_addrs_reverse.insert(fake_addr, id);
-        fake_addr
-    }
 }
 
-impl ggrs::NonBlockingSocket<SocketAddr> for WebRtcNonBlockingSocket {
-    fn send_to(&mut self, msg: &UdpMessage, addr: &SocketAddr) {
-        let id = self.fake_socket_addrs_reverse[&addr].clone();
+impl ggrs::NonBlockingSocket<String> for WebRtcNonBlockingSocket {
+    fn send_to(&mut self, msg: &UdpMessage, addr: &String) {
         let buf = bincode::serialize(&msg).unwrap();
         let packet = buf.into_boxed_slice();
-        self.socket.send(packet, id);
+        self.socket.send(packet, addr);
     }
 
-    fn receive_all_messages(&mut self) -> Vec<(SocketAddr, UdpMessage)> {
-        // let fake_socket_addrs = self.fake_socket_addrs.clone();
+    fn receive_all_messages(&mut self) -> Vec<(String, UdpMessage)> {
         let mut messages = vec![];
         for (id, packet) in self.socket.receive().into_iter() {
             let msg = bincode::deserialize(&packet).unwrap();
-            let addr = self.get_or_create_fake_addr(&id);
-            messages.push((addr, msg));
+            messages.push((id, msg));
         }
         messages
     }
-}
-
-fn make_fake_socket_addr(id: &str) -> SocketAddr {
-    // TODO: This is a horrible lazy hack, but let's just try to get it working
-    let mut hasher = DefaultHasher::new();
-    id.hash(&mut hasher);
-    let hash = hasher.finish();
-    let a: u16 = hash as u16;
-    SocketAddr::new(Ipv6Addr::new(a, a, a, a, a, a, a, a).into(), 1111)
 }

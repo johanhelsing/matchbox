bd4b2e6478af7a8470fff1ab04026619b27fc408
sapir <yasapir@gmail.com>
2022-02-02 07:39:36 +0200

    Implement ice candidate trickle

--- matchbox_socket/src/webrtc_socket/native/message_loop.rs
+++ matchbox_socket/src/webrtc_socket/native/message_loop.rs
@@ -1,14 +1,16 @@
 use bytes::Bytes;
-use futures::{pin_mut, stream::FuturesUnordered, Future, FutureExt, SinkExt, StreamExt};
+use futures::{
+    future::FusedFuture, pin_mut, stream::FuturesUnordered, Future, FutureExt, SinkExt, StreamExt,
+};
 use futures_channel::mpsc::{UnboundedReceiver, UnboundedSender};
-use futures_util::select;
+use futures_util::{lock::Mutex, select};
 use log::{debug, warn};
-use std::{collections::HashMap, sync::Arc};
+use std::{collections::HashMap, pin::Pin, sync::Arc};
 use webrtc::{
     api::APIBuilder,
     data_channel::{data_channel_init::RTCDataChannelInit, RTCDataChannel},
     ice_transport::{
-        ice_gatherer_state::RTCIceGathererState, ice_gathering_state::RTCIceGatheringState,
+        ice_candidate::{RTCIceCandidate, RTCIceCandidateInit},
         ice_server::RTCIceServer,
     },
     peer_connection::{
@@ -52,6 +54,8 @@ async fn message_loop_impl(
     messages_from_peers_tx: futures_channel::mpsc::UnboundedSender<(PeerId, Packet)>,
 ) {
     debug!("Entering native WebRtcSocket message loop");
+
+    debug!("I am {:?}", id);
 
     requests_sender
         .unbounded_send(PeerRequest::Uuid(id))
@@ -145,25 +149,95 @@ async fn message_loop_impl(
 //     res.as_ref().expect("handshake failed");
 // }
 
+struct CandidateTrickle {
+    signal_peer: SignalPeer,
+    pending: Mutex<Vec<String>>,
+}
+
+impl CandidateTrickle {
+    fn new(signal_peer: SignalPeer) -> Self {
+        Self {
+            signal_peer,
+            pending: Default::default(),
+        }
+    }
+
+    async fn on_local_candidate(
+        &self,
+        peer_connection: &RTCPeerConnection,
+        candidate: RTCIceCandidate,
+    ) {
+        let candidate = candidate.to_json().await.unwrap().candidate;
+
+        // Local candidates can only be sent after the remote description
+        if peer_connection.remote_description().await.is_some() {
+            // Can send local candidate already
+            debug!("sending IceCandidate signal {}", candidate);
+            self.signal_peer.send(PeerSignal::IceCandidate(candidate));
+        } else {
+            // Can't send yet, store in pending
+            debug!("storing pending IceCandidate signal {}", candidate);
+            self.pending.lock().await.push(candidate);
+        }
+    }
+
+    async fn send_pending_candidates(&self) {
+        let mut pending = self.pending.lock().await;
+        for candidate in std::mem::take(&mut *pending) {
+            self.signal_peer.send(PeerSignal::IceCandidate(candidate));
+        }
+    }
+
+    async fn listen_for_remote_candidates(
+        peer_connection: Arc<RTCPeerConnection>,
+        mut signal_receiver: UnboundedReceiver<PeerSignal>,
+    ) -> Result<(), Box<dyn std::error::Error>> {
+        while let Some(signal) = signal_receiver.next().await {
+            match signal {
+                PeerSignal::IceCandidate(candidate) => {
+                    debug!("got an IceCandidate signal! {}", candidate);
+                    peer_connection
+                        .add_ice_candidate(RTCIceCandidateInit {
+                            candidate,
+                            ..Default::default()
+                        })
+                        .await?;
+                }
+                PeerSignal::Offer(_) => {
+                    warn!("Got an unexpected Offer, while waiting for IceCandidate. Ignoring.")
+                }
+                PeerSignal::Answer(_) => {
+                    warn!("Got an unexpected Answer, while waiting for IceCandidate. Ignoring.")
+                }
+            }
+        }
+
+        Ok(())
+    }
+}
+
 async fn handshake_offer(
     signal_peer: SignalPeer,
     mut signal_receiver: UnboundedReceiver<PeerSignal>,
-) -> Result<(PeerId, Arc<RTCDataChannel>), Box<dyn std::error::Error>> {
+) -> Result<
+    (
+        PeerId,
+        Arc<RTCDataChannel>,
+        Pin<Box<dyn FusedFuture<Output = Result<(), Box<dyn std::error::Error>>> + Send>>,
+    ),
+    Box<dyn std::error::Error>,
+> {
     debug!("making offer");
-    let connection = create_rtc_peer_connection().await?;
+    let (connection, trickle) = create_rtc_peer_connection(signal_peer.clone()).await?;
 
     let (channel_ready_tx, mut channel_ready_rx) = futures_channel::mpsc::channel(1);
     let data_channel = create_data_channel(&connection, channel_ready_tx).await;
 
     // TODO: maybe pass in options? ice restart etc.?
     let offer = connection.create_offer(None).await?;
-
+    let sdp = offer.sdp.clone();
     connection.set_local_description(offer).await?;
-
-    wait_for_ice_complete(&connection).await;
-
-    let local_description = connection.local_description().await.unwrap();
-    signal_peer.send(PeerSignal::Offer(local_description.sdp));
+    signal_peer.send(PeerSignal::Offer(sdp));
 
     let sdp: String;
 
@@ -182,9 +256,7 @@ async fn handshake_offer(
                 warn!("Got an unexpected Offer, while waiting for Answer. Ignoring.")
             }
             PeerSignal::IceCandidate(_) => {
-                warn!(
-                    "Got an ice candidate message, but ice trickle is not yet supported. Ignoring."
-                )
+                warn!("Got an unexpected IceCandidate, while waiting for Answer. Ignoring.")
             }
         };
     }
@@ -196,17 +268,37 @@ async fn handshake_offer(
         .set_remote_description(remote_description)
         .await?;
 
-    channel_ready_rx.next().await;
+    trickle.send_pending_candidates().await;
+    let mut trickle_fut = Box::pin(
+        CandidateTrickle::listen_for_remote_candidates(connection, signal_receiver).fuse(),
+    );
 
-    Ok((signal_peer.id, data_channel))
+    let mut channel_ready_fut = channel_ready_rx.next();
+    loop {
+        select! {
+            _ = channel_ready_fut => break,
+            // TODO: this means that the signalling is down, should return an
+            // error
+            _ = trickle_fut => continue,
+        };
+    }
+
+    Ok((signal_peer.id, data_channel, trickle_fut))
 }
 
 async fn handshake_accept(
     signal_peer: SignalPeer,
     mut signal_receiver: UnboundedReceiver<PeerSignal>,
-) -> Result<(PeerId, Arc<RTCDataChannel>), Box<dyn std::error::Error>> {
+) -> Result<
+    (
+        PeerId,
+        Arc<RTCDataChannel>,
+        Pin<Box<dyn FusedFuture<Output = Result<(), Box<dyn std::error::Error>>> + Send>>,
+    ),
+    Box<dyn std::error::Error>,
+> {
     debug!("handshake_accept");
-    let connection = create_rtc_peer_connection().await?;
+    let (connection, trickle) = create_rtc_peer_connection(signal_peer.clone()).await?;
     let (channel_ready_tx, mut channel_ready_rx) = futures_channel::mpsc::channel(1);
     let data_channel = create_data_channel(&connection, channel_ready_tx).await;
     let offer;
@@ -230,20 +322,31 @@ async fn handshake_accept(
         .await?;
 
     let answer = connection.create_answer(None).await?;
-
+    signal_peer.send(PeerSignal::Answer(answer.sdp.clone()));
     connection.set_local_description(answer).await?;
+    // Can only send candidates after sending the local description.
+    trickle.send_pending_candidates().await;
+    let mut trickle_fut = Box::pin(
+        CandidateTrickle::listen_for_remote_candidates(Arc::clone(&connection), signal_receiver)
+            .fuse(),
+    );
 
-    wait_for_ice_complete(&connection).await;
+    let mut channel_ready_fut = channel_ready_rx.next();
+    loop {
+        select! {
+            _ = channel_ready_fut => break,
+            // TODO: this means that the signalling is down, should return an
+            // error
+            _ = trickle_fut => continue,
+        };
+    }
 
-    let local_description = connection.local_description().await.unwrap();
-    signal_peer.send(PeerSignal::Answer(local_description.sdp));
-
-    channel_ready_rx.next().await;
-
-    Ok((signal_peer.id, data_channel))
+    Ok((signal_peer.id, data_channel, trickle_fut))
 }
 
-async fn create_rtc_peer_connection() -> Result<RTCPeerConnection, Box<dyn std::error::Error>> {
+async fn create_rtc_peer_connection(
+    signal_peer: SignalPeer,
+) -> Result<(Arc<RTCPeerConnection>, Arc<CandidateTrickle>), Box<dyn std::error::Error>> {
     let api = APIBuilder::new().build();
 
     let config = RTCConfiguration {
@@ -258,7 +361,36 @@ async fn create_rtc_peer_connection() -> Result<RTCPeerConnection, Box<dyn std::
     };
 
     let connection = api.new_peer_connection(config).await?;
-    Ok(connection)
+    let connection = Arc::new(connection);
+
+    let trickle = Arc::new(CandidateTrickle::new(signal_peer));
+
+    let connection2 = Arc::downgrade(&connection);
+    let trickle2 = trickle.clone();
+    connection
+        .on_ice_candidate(Box::new(move |c| {
+            let connection2 = connection2.clone();
+            let trickle2 = trickle2.clone();
+            Box::pin(async move {
+                if let Some(c) = c {
+                    if let Some(connection2) = connection2.upgrade() {
+                        trickle2.on_local_candidate(&connection2, c).await;
+                    } else {
+                        warn!("missing peer_connection?");
+                    }
+                }
+            })
+        }))
+        .await;
+
+    connection
+        .on_peer_connection_state_change(Box::new(move |s| {
+            debug!("Peer Connection State has changed: {}", s);
+            Box::pin(async {})
+        }))
+        .await;
+
+    Ok((connection, trickle))
 }
 
 async fn create_data_channel(
@@ -285,45 +417,40 @@ async fn create_data_channel(
         .await;
 
     channel
-}
-
-// TODO: return result
-async fn wait_for_ice_complete(connection: &RTCPeerConnection) {
-    if connection.ice_gathering_state() == RTCIceGatheringState::Complete {
-        return;
-    }
-
-    let (mut tx, mut rx) = futures_channel::mpsc::channel(1);
-
-    connection
-        .on_ice_gathering_state_change(Box::new(move |state| {
-            if state == RTCIceGathererState::Complete {
-                tx.try_send(()).unwrap();
-            }
-            // TODO: handle failure
-            // let packet = (*message.data).into();
-            // from_peer_message_tx
-            //     .unbounded_send((peer_id.clone(), packet))
-            //     .unwrap();
+        .on_close(Box::new(move || {
+            // TODO: handle this somehow
+            debug!("Data channel closed");
             Box::pin(async move {})
         }))
         .await;
 
-    rx.next().await;
+    channel
+        .on_error(Box::new(move |e| {
+            // TODO: handle this somehow
+            warn!("Data channel error {:?}", e);
+            Box::pin(async move {})
+        }))
+        .await;
 
-    // TODO: state change handler?
-    debug!("Ice gathering completed")
+    channel
 }
 
 async fn peer_loop(
     handshake_fut: impl Future<
-        Output = Result<(PeerId, Arc<RTCDataChannel>), Box<dyn std::error::Error>>,
+        Output = Result<
+            (
+                PeerId,
+                Arc<RTCDataChannel>,
+                Pin<Box<dyn FusedFuture<Output = Result<(), Box<dyn std::error::Error>>> + Send>>,
+            ),
+            Box<dyn std::error::Error>,
+        >,
     >,
     mut new_peer_tx: UnboundedSender<PeerId>,
     from_peer_message_tx: UnboundedSender<(PeerId, Packet)>,
     mut to_peer_message_rx: UnboundedReceiver<Packet>,
 ) {
-    let (peer_id, data_channel) = handshake_fut.await.unwrap();
+    let (peer_id, data_channel, mut trickle_fut) = handshake_fut.await.unwrap();
     debug!(
         "peer_loop: sending new_peer, data channel state: {:?}",
         data_channel.ready_state()
@@ -332,7 +459,6 @@ async fn peer_loop(
     data_channel
         .on_message(Box::new(move |message| {
             let packet = (*message.data).into();
-            debug!("received message");
             from_peer_message_tx
                 .unbounded_send((peer_id.clone(), packet))
                 .unwrap();
@@ -340,10 +466,23 @@ async fn peer_loop(
         }))
         .await;
 
-    while let Some(message) = to_peer_message_rx.next().await {
-        let message = Bytes::from(message);
-        debug!("sending message");
-        data_channel.send(&message).await.unwrap();
+    let message_loop_fut = async move {
+        while let Some(message) = to_peer_message_rx.next().await {
+            let message = message.clone();
+            let message = Bytes::from(message);
+            data_channel.send(&message).await.unwrap();
+        }
+    };
+    let message_loop_fut = message_loop_fut.fuse();
+    pin_mut!(message_loop_fut);
+
+    loop {
+        select! {
+            _ = message_loop_fut => break,
+            // TODO: this means that the signalling is down, should return an
+            // error
+            _ = trickle_fut => continue,
+        }
     }
 
     // TODO: clear on_message?

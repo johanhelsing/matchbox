00521b0337a8322d6ca37a21f2242b39329c42cf
Spencer C. Imbleau, M.Sc. (he/him) <spencer@imbleau.com>
2023-02-22 10:11:35 -0500

    Update matchbox_socket/src/error.rs
    Co-authored-by: Johan Klokkhammer Helsing <johanhelsing@gmail.com>

--- matchbox_socket/src/error.rs
+++ matchbox_socket/src/error.rs
@@ -3,7 +3,7 @@ use crate::webrtc_socket::error::SignallingError;
 /// Errors that can happen when using Matchbox.
 #[derive(Debug, thiserror::Error)]
 pub enum Error {
-    /// An error ocurring during the signalling loop.
+    /// An error occurring during the signalling loop.
     #[error("An error in the signalling loop")]
     Signalling(#[from] SignallingError),
 }

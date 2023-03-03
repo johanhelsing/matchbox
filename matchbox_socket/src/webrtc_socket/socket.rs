41341e5ab3da07a35ee4b2fc342ef6e8ba2424d3
Johan Klokkhammer Helsing <johanhelsing@gmail.com>
2023-03-03 11:30:04 +0100

    Change default to only use 2 stun servers
    Fixes #119

--- matchbox_socket/src/webrtc_socket/socket.rs
+++ matchbox_socket/src/webrtc_socket/socket.rs
@@ -64,9 +64,6 @@ impl Default for RtcIceServerConfig {
             urls: vec![
                 "stun:stun.l.google.com:19302".to_string(),
                 "stun:stun1.l.google.com:19302".to_string(),
-                "stun:stun2.l.google.com:19302".to_string(),
-                "stun:stun3.l.google.com:19302".to_string(),
-                "stun:stun4.l.google.com:19302".to_string(),
             ],
             username: Default::default(),
             credential: Default::default(),

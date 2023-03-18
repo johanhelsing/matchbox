e1d011dc7269539f50483481ddc153ae035fca41
Spencer C. Imbleau <spencer@imbleau.com>
2023-03-18 17:20:22 -0400

    cleanup

--- matchbox_signalling_socket/src/error.rs
+++ matchbox_signalling_socket/src/error.rs
@@ -1,11 +1,11 @@
 use crate::signalling_socket::error::SignalingError;
 
-/// Errors that can happen when using Matchbox.
+/// Errors that can happen when using a signaling server.
 #[derive(Debug, thiserror::Error)]
 pub enum Error {
     /// An error occurring during the signalling loop.
     #[error("An unrecoverable error in the signalling loop")]
-    Server(#[from] SignalingError),
+    Signaling(#[from] SignalingError),
 
     /// An error occurring from hyper
     #[error("Hyper error")]

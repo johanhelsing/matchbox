b496eba0bdcd6693567a87d7feb4a7ee1fe92694
Garry O'Donnell <garry.euan.odonnell@ntlworld.com>
2023-07-02 12:01:26 +0100

    Move task handle into bevy Resource / Component

--- bevy_matchbox/src/socket.rs
+++ bevy_matchbox/src/socket.rs
@@ -1,13 +1,16 @@
 use bevy::{
     ecs::system::Command,
-    prelude::{Commands, Component, Deref, DerefMut, Resource, World},
-    tasks::IoTaskPool,
+    prelude::{Commands, Component, Resource, World},
+    tasks::{IoTaskPool, Task},
 };
 pub use matchbox_socket;
 use matchbox_socket::{
     BuildablePlurality, MessageLoopFuture, SingleChannel, WebRtcSocket, WebRtcSocketBuilder,
 };
-use std::marker::PhantomData;
+use std::{
+    marker::PhantomData,
+    ops::{Deref, DerefMut},
+};
 
 /// A [`WebRtcSocket`] as a [`Component`] or [`Resource`].
 ///
@@ -65,8 +68,25 @@ use std::marker::PhantomData;
 ///     commands.remove_resource::<MatchboxSocket<SingleChannel>>();
 /// }
 /// ```
-#[derive(Resource, Component, Debug, Deref, DerefMut)]
-pub struct MatchboxSocket<C: BuildablePlurality>(WebRtcSocket<C>);
+#[derive(Resource, Component, Debug)]
+pub struct MatchboxSocket<C: BuildablePlurality>(
+    WebRtcSocket<C>,
+    Task<Result<(), matchbox_socket::Error>>,
+);
+
+impl<C: BuildablePlurality> Deref for MatchboxSocket<C> {
+    type Target = WebRtcSocket<C>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl<C: BuildablePlurality> DerefMut for MatchboxSocket<C> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
+    }
+}
 
 impl<C: BuildablePlurality> From<WebRtcSocketBuilder<C>> for MatchboxSocket<C> {
     fn from(builder: WebRtcSocketBuilder<C>) -> Self {
@@ -77,8 +97,8 @@ impl<C: BuildablePlurality> From<WebRtcSocketBuilder<C>> for MatchboxSocket<C> {
 impl<C: BuildablePlurality> From<(WebRtcSocket<C>, MessageLoopFuture)> for MatchboxSocket<C> {
     fn from((socket, message_loop_fut): (WebRtcSocket<C>, MessageLoopFuture)) -> Self {
         let task_pool = IoTaskPool::get();
-        task_pool.spawn(message_loop_fut).detach();
-        MatchboxSocket(socket)
+        let task = task_pool.spawn(message_loop_fut);
+        MatchboxSocket(socket, task)
     }
 }
 
